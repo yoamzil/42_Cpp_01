f7584b0974112213e8fe32e6fd8a915bcc9e4658
Youness Amzil <amzilyouness@gmail.com>
2023-10-17 21:23:48 +0100

    Working...

---
+++ ex03/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************************** */
+/*																						  */
+/*                                                						d8b		888       */
+/*                                                  					Y8P		888       */
+/*                                                      						888       */
+/*		888  888	  .d88b.   	8888b.  	88888b.d88b.	88888888	888 	888       */
+/*		888  888 	d88""88b     	"88b 	888 "888 "88b    	d88P 	888 	888       */
+/*		888  888 	888  888 	.d888888 	888  888  888		d88P   	888 	888       */
+/*		Y88b 888 	Y88..88P 	888  888 	888  888  888	  d88P    	888 	888       */
+/*		"Y88888  	"Y88P"  	"Y888888 	888  888  888	88888888 	888 	888       */
+/*	  		 888																          */
+/*		Y8b d88P																          */
+/*		"Y88P"									By: yoamzil <yoamzil@student.1337.ma>     */
+/*																						  */
+/* ************************************************************************************** */
+
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main(void)
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	// {
+	// 	Weapon club = Weapon("crude spiked club");
+	// 	HumanB jim("Jim");
+	// 	jim.setWeapon(club);
+	// 	jim.attack();
+	// 	club.setType("some other type of club");
+	// 	jim.attack();
+	// }
+	return (0);
+}
