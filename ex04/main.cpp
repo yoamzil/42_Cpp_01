6ab27da638f1a81136629458e4bd57abb9c0153a
Youness Amzil <amzilyouness@gmail.com>
2023-10-18 20:00:20 +0100

    Getting there...

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -14,3 +14,51 @@
 /*																						  */
 /* ************************************************************************************** */
 
+#include "file.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		std::string		source = av[1];
+		std::string		destination = source + ".replace";
+
+		std::ifstream	sourceFile(source);
+		std::ofstream	destinationFile(destination);
+
+		if (!sourceFile.is_open() || !destinationFile.is_open())
+		{
+			std::cerr << "Could not open files for copying." << std::endl;
+			return (1);
+		}
+
+		std::string		s1 = av[2];
+		std::string		s2 = av[3];
+		std::string		line;
+		size_t			index;
+
+		while (std::getline(sourceFile, line))
+		{
+			index = line.find(s1);
+			while (index != std::string::npos)
+			{
+				line.erase(index, s1.length());
+				line.insert(index, s2);
+				index = line.find(s1, index + s2.length());
+			}
+			destinationFile << line << '\n';
+		}
+		// if (!std::getline(sourceFile, line))
+		// {
+		// 	std::cout << "EMPTY FILE." << std::endl;
+		// 	return (1);
+		// }
+		sourceFile.close();
+		destinationFile.close();
+
+		std::cout << "File copied successfully." << std::endl;
+	}
+	else
+		std::cout << "Wrong number of arguments" << std::endl;
+	return 0;
+}
