82f6dfba63e4e8168a215e49f7f0bad0906a62e6
Youness Amzil <amzilyouness@gmail.com>
2023-10-19 17:08:04 +0100

    Finished.

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -22,7 +22,16 @@
 class	Harl
 {
 	private:
+
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
 	public:
+
+		void	complain(std::string level);
+
 };
 
 #endif
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -16,5 +16,13 @@
 
 #include "Harl.hpp"
 
-int	main()
-{}
+int	main(int ac, char **av)
+{
+	Harl	obj;
+
+	if (ac == 2)
+		obj.complain(av[1]);
+	else
+		std::cout << "TRY AGAIN!" << std::endl;
+	return (0);
+}
